0090f5a3fe094fdb87b9e38339c1d3a9944e9629
Bangash-1122 <ubaidkhan0077800@gmail.com>
2025-12-11 19:47:01 -0500

    feat: initialize Tailwind CSS project with Vite and React
    - Added package.json with dependencies for React, Vite, Tailwind CSS, and ESLint.
    - Configured PostCSS with Tailwind CSS and Autoprefixer.
    - Created initial SVG assets for Vite and React logos.
    - Developed App component with Tailwind CSS styling and Card component for user interaction.
    - Implemented global styles in index.css using Tailwind directives.
    - Set up main entry point in main.jsx for rendering the App component.
    - Configured Tailwind CSS with content paths for purging unused styles.
    - Created Vite configuration file for project setup.
    - Added custom rendering function for a simple React-like implementation.
    - Created an HTML file to serve the custom React application.

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'; 
+import './App.css'
+
+function App() {
+
+  const [ counter, setCounter] = useState(15) 
+
+  // let counter = 15
+
+  const addValue = () => { 
+    // counter + 1;
+
+    setCounter(counter + 1)
+    
+  }
+
+  const removeValue = () => {
+    setCounter(counter -1)
+  }
+ 
+
+  return (
+    <>
+      <h1>Chair aur react !</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove value</button>
+    </>
+  )
+}
+
+export default App
